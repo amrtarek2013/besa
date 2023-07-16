0e45a923c82d5a2fec410904b83fe0b1cde6148e
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-07-16 18:03:43 +0300

    Country Modules

--- templates/element/flash/default.php
+++ templates/element/flash/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var array $params
@@ -12,4 +13,21 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="<?= h($class) ?>" onclick="this.classList.add('hidden');"><?= $message ?></div>
+<!-- <div class="<?= h($class) ?>" onclick="this.classList.add('hidden');"><?= $message ?></div> -->
+
+<!-- start remodal sections  -->
+<div class="remodal comfirmDetails successModal" data-remodal-id="FlashMessagePop" id="FlashMessagePop" data-remodal-options="hashTracking: false, closeOnOutsideClick" style="display:none;">
+    <div class="CDhead">
+        <h2></h2>
+        <button data-remodal-action="close" class="remodal-close"></button>
+    </div><!-- end head  -->
+    <div class="CDbody">
+        <div class="successSection">
+            <img src="" alt="" class="showHideSuccess">
+            <p>
+                <?= $message ?>
+            </p>
+        </div>
+
+    </div><!-- end body  -->
+</div>
