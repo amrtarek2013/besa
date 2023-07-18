9e0bb48302a826d7f39b82ba14d820ebbd8a842b
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-07-18 03:06:38 +0300

    Contact - Register - login - forgot - emails

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -42,7 +42,7 @@
     $(document).ready(function() {
         toastr.options.closeButton = true;
         toastr.options.closeMethod = 'fadeOut';
-        toastr.options.closeDuration = 300;
+        toastr.options.closeDuration = 500;
         toastr.options.closeEasing = 'swing';
         if ($(".message.success").length) {
             toastr.success($(".message.success").text());
