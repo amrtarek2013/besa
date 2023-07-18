b0aa90ed94ed299300d71fc92f0d26c58a689613
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-07-18 14:26:35 +0300

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Pages/partnership_with_besa.php
+++ templates/Pages/partnership_with_besa.php
@@ -40,7 +40,7 @@
                 </div>
             </div>
            -->
-            <div class="col-md-12" style="padding: 0; background:#fff">
+            <div class="col-md-12" style="padding: 0;">
                 <div class="line-stained">
                     <span></span>
                     <span></span>
