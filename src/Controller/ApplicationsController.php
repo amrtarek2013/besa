077916a7f85a7ac3d2b3fd9f6809575f176c7ae8
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-07-19 19:06:18 +0300

    Application

--- src/Controller/ApplicationsController.php
+++ src/Controller/ApplicationsController.php
@@ -121,7 +121,7 @@ class ApplicationsController extends AppController
             // $application->user_id = $user['id'];
         }
 
-        $this-loadModel('ApplicationCourses');
+        $this->loadModel('ApplicationCourses');
         $applicationCourse = $this->ApplicationCourses->newEmptyEntity();
         if ($course) {
             if ($isNew == 'add') {
