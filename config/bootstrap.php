dfd458ca6c85d705d9a107185405237d6209a234
KG <developerkg@thesitefactory.com.au>
2023-07-19 19:24:44 +0200

    working on study page

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -89,7 +89,7 @@ try {
  * Load an environment local configuration file to provide overrides to your configuration.
  * Notice: For security reasons app_local.php **should not** be included in your git repo.
  */
-if (file_exists(CONFIG . 'app_local.php')) {
+if (file_exists(CONFIG . 'app.php')) {
     Configure::load('app', 'default');
 }
 
