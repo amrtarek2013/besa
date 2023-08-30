6cb642f38ea44513f3b7f2409d4e6b1529818da4
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-08-31 01:30:27 +0300

    Fix urls live

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1020,7 +1020,7 @@ class AppController extends Controller
             'https://besa.intimedev.com'
         );
 
-        debug($_SERVER['REMOTE_ADDR']);
+        debug($_SERVER);
        
         if (!(!empty($_SERVER['REMOTE_ADDR']) && in_array($_SERVER['REMOTE_ADDR'], $local))) {
             $this->loadComponent('EmailSender');
