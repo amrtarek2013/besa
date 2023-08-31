4750e8c71dc34ae33b0372ca14259c9052b979e8
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-08-31 21:24:05 +0300

    Fix urls live

--- templates/Universities/index.php
+++ templates/Universities/index.php
@@ -45,19 +45,18 @@
                                 ?>
 
                             <?php endforeach; ?>
-
-                            <div class="paginator">
-                                <ul class="pagination">
-                                    <?= $this->Paginator->first(' << ') ?>
-                                    <?= $this->Paginator->prev(' < ') ?>
-                                    <?= $this->Paginator->numbers() ?>
-                                    <?= $this->Paginator->next(' > ') ?>
-                                    <?= $this->Paginator->last(' >> ') ?>
-                                </ul>
-                                <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-                            </div>
                         </div>
 
+                        <div class="paginator">
+                            <ul class="pagination">
+                                <?= $this->Paginator->first(' << ') ?>
+                                <?= $this->Paginator->prev(' < ') ?>
+                                <?= $this->Paginator->numbers() ?>
+                                <?= $this->Paginator->next(' > ') ?>
+                                <?= $this->Paginator->last(' >> ') ?>
+                            </ul>
+                            <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+                        </div>
                     <?php endif; ?>
 
                     <!-- <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
