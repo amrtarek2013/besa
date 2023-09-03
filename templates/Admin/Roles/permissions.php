27df8eaace1c0808bf6eeb84972c12b838bef338
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-03 23:37:25 +0300

    Cache menus/snippets/courses/permissions/...

--- templates/Admin/Roles/permissions.php
+++ templates/Admin/Roles/permissions.php
@@ -1,67 +1,85 @@
+<style>
+    .form-group {
+        margin-bottom: 27px;
+        width: 100%;
+        clear: both;
+    }
+
+    input[type="checkbox"],
+    input[type="radio"] {
+
+        float: left;
+    }
+
+    label:not(.form-check-label):not(.custom-file-label) {
+        float: left;
+        margin-top: -5px;
+    }
+</style>
 <div class="content-wrapper">
-    
-<section class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1><?=__('Permission Permissions')?></h1>
-            </div>
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="/">Home</a></li>
-                    <li class="breadcrumb-item"><a href="<?=ADMIN_LINK?>/permissions">Permissions</a></li>
-                </ol>
-            </div>
-        </div>
-    </div>
-</section>
 
-<section class="content">
-    <div class="container-fluid">
-        <div class="row">
-        <div class="col-md-12">
-        
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title"><?= __(ucfirst($this->getRequest()->getParam('action')) ) ?></h3>
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1><?= __('Permission Permissions') ?></h1>
                 </div>
-
-                 <?= $this->AdminForm->create(null, ['type' => 'file']) ?>
-                    <div class="card-body">
-                        
-                        <?php
-                        $current_group = "";
-                        foreach ($all_cer_permissions as $key => $value) {
-                            if(empty($current_group) || $value->permission_group != $current_group){
-                                $current_group = $value->permission_group;
-                                ?>
-                                <div class="form-group">
-                                    <input type="checkbox" name="<?=$current_group?>" value="<?=$current_group?>" class="main_group">
-                                    <label for="title"><?=__($current_group)?></label>
-                                </div>
-                                <?php
-                            }
-                        ?>
-                            <div class="form-group mg-<?=$currLang=='en'?'left':'right'?>-40">
-                                <input type="checkbox" name="permissions[<?=$value->id?>]" class="" data-permission-group="<?=$current_group?>" value="<?=$value->id?>" <?=in_array($value->id,$saved_perm)?'checked':''?> >
-                                <label for="title"><?=$currLang=='en'?$value->title:$value->title_ar?></label>
-                            </div>
-                        <?php } ?>
-                        
-                    </div>
-
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary"><?=__('Submit')?></button>
-                    </div>
-                <?= $this->AdminForm->end() ?>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item"><a href="<?= ADMIN_LINK ?>/permissions">Permissions</a></li>
+                    </ol>
+                </div>
             </div>
-            <!-- /.card -->
+        </div>
+    </section>
 
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title"><?= __(ucfirst($this->getRequest()->getParam('action'))) ?></h3>
+                        </div>
+
+                        <?= $this->AdminForm->create(null, ['type' => 'file']) ?>
+                        <div class="card-body">
+                            <div class="form-group" style="color:blue; font-size:20px">
+                                <input type="checkbox" name="all_group" value="1" class="all_group">
+                                <label for="title"><?= __('Check/Uncheck All') ?></label>
+                            </div>
+                            <?php
+                            $current_group = "";
+                            foreach ($all_cer_permissions as $key => $value) {
+                                if (empty($current_group) || $value->permission_group != $current_group) {
+                                    $current_group = $value->permission_group;
+                            ?>
+                                    <div class="form-group">
+                                        <input type="checkbox" name="<?= $current_group ?>" value="<?= $current_group ?>" class="main_group" data-all-permission-group="all-permissions">
+                                        <label for="title"><?= __($current_group) ?></label>
+                                    </div>
+                                <?php
+                                }
+                                ?>
+                                <div class="form-group mg-<?= $currLang == 'en' ? 'left' : 'right' ?>-40"  style="margin-left:20px">
+                                    <input type="checkbox" name="permissions[<?= $value->id ?>]" class="" data-permission-group="<?= $current_group ?>" data-all-permission-group="all-permissions" value="<?= $value->id ?>" <?= in_array($value->id, $saved_perm) ? 'checked' : '' ?>>
+                                    <label for="title"><?= $currLang == 'en' ? $value->title : $value->title_ar ?></label>
+                                </div>
+                            <?php } ?>
+
+                        </div>
+
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary"><?= __('Submit') ?></button>
+                        </div>
+                        <?= $this->AdminForm->end() ?>
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
-</section>
+    </section>
 </div>
-
-
-
--- webroot/Admin/custom_js.js
+++ webroot/Admin/custom_js.js
@@ -1,78 +1,101 @@
-$(".delete-action").click(function(){
+$(".delete-action").click(function () {
 	return confirm('Are you sure you want to delete this item?');
 });
 
-$('.main_group').change(function() {
+$('.main_group').change(function () {
 	var permission_group = $(this).val();
-    if(this.checked) {
-        $('input[data-permission-group="'+permission_group+'"]').prop('checked', true);
-    }else{
-        $('input[data-permission-group="'+permission_group+'"]').prop('checked', false);
-    }
+	if (this.checked) {
+		$('input[data-permission-group="' + permission_group + '"]').prop('checked', true);
+	} else {
+		$('input[data-permission-group="' + permission_group + '"]').prop('checked', false);
+	}
 
 });
 
-$(document).ready(function(){
-	$('.main_group').each(function(){
+$(document).ready(function () {
+	$('.main_group').each(function () {
 		var permission_group = $(this).val();
 		var all_checked = true;
-		$('input[data-permission-group="'+permission_group+'"]').each(function(){
-			if(this.checked) {
-			}else{
+		$('input[data-permission-group="' + permission_group + '"]').each(function () {
+			if (this.checked) {
+			} else {
 				all_checked = false;
 
 			}
 		});
 		// alert(all_checked)
-		if(all_checked){
+		if (all_checked) {
 			$(this).prop('checked', true);
 		}
 	});
 });
 
-$('#all_os').change(function() {
-    if(this.checked) {
-        $('.os_item').prop('checked', true);
-    }else{
-        $('.os_item').prop('checked', false);
-    }
+$('.all_group').change(function () {
+	if (this.checked) {
+		$('input[data-all-permission-group="all-permissions"]').prop('checked', true);
+	} else {
+		$('input[data-all-permission-group="all-permissions"]').prop('checked', false);
+	}
+
+});
+
+$(document).ready(function () {
+	let all_checked = true;
+	$('input[data-all-permission-group="all-permissions"]').each(function () {
+		if (this.checked) {
+		} else {
+			all_checked = false;
+		}
+	});
+	// alert(all_checked)
+	if (all_checked) {
+		$('.all_group').prop('checked', true);
+	}
+});
+
+$('#all_os').change(function () {
+	if (this.checked) {
+		$('.os_item').prop('checked', true);
+	} else {
+		$('.os_item').prop('checked', false);
+	}
 });
 var active_page_url = window.location.href;
-$('.nav-link2').each(function(){
+$('.nav-link2').each(function () {
 	// console.log($(this).attr("href"))
-	if($(this).attr("href")==active_page_url){
+	if ($(this).attr("href") == active_page_url) {
 		$(this).addClass("active2");
 	}
 });
 
 
-$(".custom_sorting").click(function(e){
+$(".custom_sorting").click(function (e) {
 	e.preventDefault();
 	var sort_value = $(this).data("sort");
-	var dir_value  = $(this).data("dir");
+	var dir_value = $(this).data("dir");
 	$("#sort_value").val(sort_value);
 	$("#dir_value").val(dir_value);
 
 	$("#export_value").val("");
-	
-    $("#filter_form").submit();
-}); 
+
+	$("#filter_form").submit();
+});
 
 
-$('#enable_random_selection').change(function() {
+$('#enable_random_selection').change(function () {
 
-    if(this.checked) {
-    	var check_val = 1;
-    }else{
-    	var check_val = 0;    
-    }
-    $.ajax({
-        type: "POST",
-        url: "https://rdidt.com/dashboard/admin/general-configurations/enable-random-selection",
-        data: {'_csrfToken': _csrfToken,"check":check_val},
-        dataType: 'json',
-    }).done(function(data) {
-        toastr.success("Your settings saved successfully.");
-    });
+	if (this.checked) {
+		var check_val = 1;
+	} else {
+		var check_val = 0;
+	}
+	$.ajax({
+		type: "POST",
+		url: "https://rdidt.com/dashboard/admin/general-configurations/enable-random-selection",
+		data: { '_csrfToken': _csrfToken, "check": check_val },
+		dataType: 'json',
+	}).done(function (data) {
+		toastr.success("Your settings saved successfully.");
+	});
 
 });
