e0933f79598daf7478ac56f02ea6477b71a847b9
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-04 20:48:27 +0300

    Cache menus/snippets/courses/permissions/...

--- templates/element/user-side-menu copy.php
+++ templates/element/user-side-menu copy.php
@@ -46,43 +46,42 @@
         margin-bottom: 0;
         list-style: none;
     }
-
-    .nav li {
+    .nav li{
         width: 100%;
-        clear: both;
-        padding-bottom: 15px;
+        clear:both;
+        padding-bottom:15px;
     }
 </style>
-<aside class="main-sidebar sidebar-dark-primary elevation-4 counselor-sidebar">
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="<?= USER_LINK ?>" class="brand-link">
-        <span class="brand-text font-weight-light"><?= __('Student Dashboard') ?></span>
+        <!-- <img src="<?= ADMIN_ASSETS ?>/dist/img/AdminLTELogo.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
+        <span class="brand-text font-weight-light"><?= __('User Dashboard') ?></span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="left-box">
-                <div class="circle-img circle-img-center">
-                    <img src="<?= $logged_user_info['image_path'] ?>" alt="" style="width: 128px;height: 128px;">
-                </div>
-                <!-- <h4 class="name-profile"><?= strtoupper($auth->user('first_name') . ' ' . $auth->user('last_name')) ?></h4> -->
-                <?php
-
-                use Cake\Routing\Router;
-
-                ?>
-                <span class="online-status"><?= true ? 'ONLINE' : 'OFFLINE' ?></span>
-
+            <div class="image">
+                <!-- <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image"> -->
             </div>
+            <!-- <div class="info">
+                <a href="#" class="d-block"><?= __($g_configs['general']['txt.site_name'] . ' System v 1.0 ') ?></a>
+            </div> -->
         </div>
 
+        <!-- SidebarSearch Form -->
+        <!-- <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="<?= __('Search') ?>" aria-label="<?= __('Search') ?>">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar"><i class="fas fa-search fa-fw"></i></button>
+                </div>
+            </div>
+        </div> -->
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <?= $this->AdminSideMenu->render($sideMenus, $current_prefix, true); ?>
-            <div class="info">
-                <a href="<?= Router::url('/applications') ?>" class="Track-App"><?= __('Track & view your application') ?></a>
-            </div>
         </nav>
         <!-- /.sidebar-menu -->
 
