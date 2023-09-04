d512cd443a4106eee0a8b0e6f2d7d04551b9ad23
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-04 14:29:45 +0300

    Cache menus/snippets/courses/permissions/...

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -7,6 +7,8 @@
     <title><?= isset($title) ? $g_configs['general']['txt.site_name'] . ' - ' . $title : $g_configs['general']['txt.site_name'] . ' - British educational services agency' ?></title>
     <link rel="icon" type="image/x-icon" href="<?= FRONT_ASSETS ?>/favicon.ico">
     <!-- add This File (range slider)-->
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.0.2/nouislider.min.js" as="script">
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.0.2/nouislider.min.css" as="style">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.0.2/nouislider.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.0.2/nouislider.min.css" />
     <?= $this->Html->css([
@@ -54,6 +56,10 @@
     <link rel="preload" as="image" href="<?= WEBSITE_URL ?>img/hero-bg3.png">
     <link rel="preload" as="image" href="<?= WEBSITE_URL ?>img/Welcome-vis.png">
     <link rel="preload" as="image" href="<?= WEBSITE_URL ?>img/dots-153.png">
+
+    <link rel="preload" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" as="style" />
+
+    <link rel="preload" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js" as="script" />
 </head>
 <?php $bodyClass = isset($bodyClass) ? $bodyClass : '' ?>
 
