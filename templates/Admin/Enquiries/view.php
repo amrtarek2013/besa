327b739aca5317ac79b190affcd0ef0fc6ca0b3a
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-05 02:10:53 +0300

    Cache menus/snippets/courses/permissions/...

--- templates/Admin/Enquiries/view.php
+++ templates/Admin/Enquiries/view.php
@@ -48,7 +48,8 @@
                                             $enquiry[$field] = ($field == 'destination_id') ? $enquiry['country']['country_name'] : $enquiry[$field];
                                             if ($enquiry['type'] == 'book-appointment') {
                                                 if (isset($_GET['dk']))
-                                                    var_dump($enquiry);
+                                                    var_dump($interestedStudyLevels);
+                                                    
                                                 $enquiry[$field] = ($field == 'study_level' && isset($interestedStudyLevels)) ? $interestedStudyLevels[$enquiry[$field]] : $enquiry[$field];
                                             }
                                             ?>
