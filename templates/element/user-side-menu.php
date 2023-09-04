85e4300699d9474ccdedc2668f7695643bc582b3
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-04 20:49:18 +0300

    Cache menus/snippets/courses/permissions/...

--- templates/element/user-side-menu.php
+++ templates/element/user-side-menu.php
@@ -80,9 +80,7 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <?= $this->AdminSideMenu->render($sideMenus, $current_prefix, true); ?>
-            <div class="info">
-                <a href="<?= Router::url('/applications') ?>" class="Track-App"><?= __('Track & view your application') ?></a>
-            </div>
+           
         </nav>
         <!-- /.sidebar-menu -->
 
