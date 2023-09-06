0670c72e2f34f2a930245f9bcae612825aa4e846
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-07 01:28:38 +0300

    Cache menus/snippets/courses/permissions/...

--- templates/Blogs/details.php
+++ templates/Blogs/details.php
@@ -7,10 +7,10 @@
                 <img src="<?= $blog['header_image_path'] ?>" alt="">
             </div>
             <div class="col-md-12">
-                <div class="relative-box-about ">
-                    <h1 class="relative-text"><?= $blog['title'] ?></h1>
-                    <h2 class="title text-left"><?= $blog['short_text'] ?></h2>
-                </div>
+                <!-- <div class="relative-box-about "> -->
+                    <!-- <h1 class="relative-text"><?= $blog['title'] ?></h1> -->
+                    <h2 class="title text-left"><?= $blog['title'] ?></h2>
+                <!-- </div> -->
             </div>
         </div>
     </div>
