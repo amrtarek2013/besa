5d0ef54a0c74e86f5665825cffab838a33fa5899
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-07 19:44:15 +0300

    Cache menus/snippets/courses/permissions/...

--- templates/Blogs/index.php
+++ templates/Blogs/index.php
@@ -3,8 +3,8 @@
         <div class="row">
             <div class="col-md-7">
                 <div class="background-banner-color">
-                    <img src="<?=WEBSITE_URL?>img/hero-bg45.png" alt=""  width="">
-                    <img src="<?=WEBSITE_URL?>img/dots-153.png" width="" alt="" class="relative-dots-about">
+                    <img src="<?= WEBSITE_URL ?>img/hero-bg45.png" alt="" width="">
+                    <img src="<?= WEBSITE_URL ?>img/dots-153.png" width="" alt="" class="relative-dots-about">
                 </div>
             </div>
             <div class="col-md-5">
@@ -24,7 +24,7 @@
 
 
                     <?php
- if (!empty($blogs)) : ?>
+                    if (!empty($blogs)) : ?>
                         <?php $counter = 0; ?>
 
                         <div class="display-flex">
@@ -34,7 +34,7 @@
                                     <img src="<?= $blog['image_path'] ?>" alt="This Is Blog Img 1" loading="lazy">
                                     <div class="content-blog">
                                         <p><?= $blog['title'] ?></p>
-                                        <!-- <a href="<?= Cake\Routing\Router::url('/'.$g_dynamic_routes['blogs.details'].'/'. $blog['permalink']) ?>" class="read-anchor">READ MORE</a> -->
+                                        <a href="<?= Cake\Routing\Router::url('/' . $g_dynamic_routes['blogs.details'] . '/' . $blog['permalink']) ?>" class="read-anchor">READ MORE</a>
                                     </div>
                                 </div>
 
@@ -46,7 +46,25 @@
 
                             <?php endforeach; ?>
 
+                            <?php
+                            if ($counter % 2 != 0) {
+                                echo '<div class="box-blog display-flex"></div>';
+                            }
+                            ?>
                         </div>
+
+                        <?php if (sizeof($universities) == 20) : ?>
+                            <div class="paginator">
+                                <ul class="pagination">
+                                    <?= $this->Paginator->first(' << ') ?>
+                                    <?= $this->Paginator->prev(' < ') ?>
+                                    <?= $this->Paginator->numbers() ?>
+                                    <?= $this->Paginator->next(' > ') ?>
+                                    <?= $this->Paginator->last(' >> ') ?>
+                                </ul>
+                                <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+                            </div>
+                        <?php endif; ?>
                     <?php endif; ?>
                 </div>
             </div>
