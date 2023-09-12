259c09fecf297915d68e125dc700d67e7b471422
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-09-12 16:19:47 +0300

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Events/school_tours.php
+++ templates/Events/school_tours.php
@@ -1,13 +1,13 @@
 <section class="main-banner british-banner fair-banner <?= $event['style'] ?>" style="padding-bottom:0 !important;">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-7">
                 <div class="background-banner-color">
                     <img src="/img/31098 [Converted] 1.png" alt="" style="z-index: 2;" width="">
                     <img src="<?= WEBSITE_URL ?>img/dots-153.png" width="" alt="" class="relative-dots-about">
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-5">
                 <div class="relative-box-about ">
 
                     <?php $tt = explode(' ', trim($event['title'])) ?>
@@ -18,7 +18,20 @@
             </div>
         </div>
     </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12" style="padding: 0;">
+                <div class="title-banner-blue  title-banner-green" >
+                    <h2>Dreaming of studying abroad?</h2>
+                    <p>
+                        The journey from the MENA region to international
+                        universities is an exciting one, and the International General Certificate of Secondary
+                        Education (IGCSE) can be your passport to this adventure.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 
-1
