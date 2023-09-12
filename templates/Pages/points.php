8aea50f47ba92fff6b22ada1c4cae67cb37e1b55
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-12 15:41:46 +0300

    Cache menus/snippets/courses/permissions/...

--- templates/Pages/points.php
+++ templates/Pages/points.php
@@ -1,22 +1,19 @@
-<section class="main-banner  whereToStudy-banner">
-    <div class="container-fluid">
-        <div class="row">
+<section class="main-banner register-banner Create-account-banner">
 
-            <div class="col-md-6">
-                <div class="relative-box-about ">
-                    <h1 class="relative-text">Points</h1>
-                    <h2 class="title text-left">Counselor<br/>
-                        Points</h2>
-                    <!-- <p class="relative-textP">BESA Can Help You Study in Over 10 Countries</p> -->
-                </div>
-            </div>
+    <div class="container">
+        <div class="row">
             <div class="col-md-6">
                 <div class="background-banner-color">
                     <img src="<?= WEBSITE_URL ?>img/Resume-2 1.png" alt="" width="">
                     <img src="<?= WEBSITE_URL ?>img/dots-153.png" width="" alt="" class="relative-dots-about">
                 </div>
             </div>
+            <div class="col-md-6">
+                <div class="relative-box-about ">
+                    <h1 class="relative-text">Points</h1>
+                    <h2 class="title text-left">Counselor Points</h2>
+                </div>
+            </div>
         </div>
-
     </div>
 </section>
