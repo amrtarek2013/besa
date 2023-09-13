e00ef36c07f92edd88650bfcaac3501cf474beaa
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-09-13 13:57:08 +0300

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Pages/points.php
+++ templates/Pages/points.php
@@ -1,8 +1,8 @@
 <section class="main-banner register-banner Create-account-banner">
 
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-7">
                 <div class="background-banner-color">
                     <img src="<?= WEBSITE_URL ?>img/Resume-2 1.png" alt="" width="">
                     <img src="<?= WEBSITE_URL ?>img/dots-153.png" width="" alt="" class="relative-dots-about">
@@ -12,6 +12,28 @@
                 <div class="relative-box-about ">
                     <h1 class="relative-text">Points</h1>
                     <h2 class="title text-left">Counselor<br /> Points</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12" style="padding: 0;">
+                <div class="title-banner-blue  title-banner-green">
+                    <div class="grid-container-3col  ">
+                        <div class="box-point">
+                            <h3 class="numb">5</h3>
+                            <p>Joined Successfully</p>
+                        </div>
+                        <div class="box-point">
+                            <h3 class="numb">25</h3>
+                            <p>Applied</p>
+                        </div>
+                        <div class="box-point">
+                            <h3 class="numb">55</h3>
+                            <p>Points Acquired</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
