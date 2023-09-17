b53bb2a8e1247354ede4ac3019bf5a10cd024816
KG <developerkg@thesitefactory.com.au>
2023-09-18 02:44:26 +0300

    bitrix 24 integration

--- plugins/bitrix/BitrixIntegration.php
+++ plugins/bitrix/BitrixIntegration.php
@@ -15,23 +15,31 @@ class BitrixIntegration{
 	public function add_lead_from_book_appointment($data,$type){
 		// $options =  [];
 		// $result = $this->bitrix_process_api('crm.lead.list',$options);
-		$options =  [
-					'fields' => [
-				        'TITLE' => 'KG-Lead',
-				        'NAME' => 'kkkkarim',
-				        'SECOND_NAME' => 'GGGGGamal',
-				        'LAST_NAME' => 'GGGGGamal',
-				        'STATUS_ID' => 'NEW',
-						// "ASSIGNED_BY_ID": 1, 
-    					"OPENED"=> "Y", 
-						"PHONE"=> [ [ "VALUE"=> "555888","VALUE_TYPE"=> "WORK" ] ] ,
+		$data_arr = [];
+		if(!empty($data['name'])){
+			$data_arr['TITLE'] = $data['name'];
+			$name_arr = $this->get_name_info($data['name']);
+		}
+		if(!empty($name_arr['first_name'])){
+			$data_arr['NAME'] = $name_arr['first_name'];
+		}
+		if(!empty($name_arr['second_name'])){
+			$data_arr['SECOND_NAME'] = $name_arr['second_name'];
+		}
+		if(!empty($name_arr['last_name'])){
+			$data_arr['LAST_NAME'] = $name_arr['last_name'];
+		}
+		$data_arr['STATUS_ID'] = 'NEW';
+		$data_arr['OPENED'] = 'Y';
+		// $data_arr['PHONE'] = [ [ "VALUE"=> "555888","VALUE_TYPE"=> "WORK" ] ];
+		if(!empty($data_arr['TITLE'])){
+			$options =  ['fields' => $data_arr,];
 
-				    ],
-				];
-		$result = $this->bitrix_process_api('crm.lead.add',$options);
-		echo '<pre>';
-		print_r($result);
-		echo '</pre>';die;
+			$result = $this->bitrix_process_api('crm.lead.add',$options);
+			echo '<pre>';
+			print_r($result);
+			echo '</pre>';die;
+		}
 	}
 	function bitrix_process_api($endpoint,$options){
 		require_once ($this->main_dir.'/plugins/bitrix/crest.php');
@@ -42,6 +50,24 @@ class BitrixIntegration{
 		    return ['status'=>true,'data'=>$response['result']];
 		}
 		return ['status'=>false,'message'=>'Invalid Request'];
+	}
+	function get_name_info($name){
+		$return = [];
+		if(empty($name)){
+			return [];
+		}
+		$name_segments = explode($name);
+		if(!empty($name_segments[0])){
+			$return['first_name'] = $name_segments[0];
+		}
+		if(!empty($name_segments[1])){
+			$return['second_name'] = $name_segments[1];
+		}
+		if(!empty($name_segments[2])){
+			$return['last_name'] = $name_segments[2];
+		}
+
+		return $return;
 	}
 }
 //------------------------------------------
