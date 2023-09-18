8a0e8d41ee926270a820e25aa9d0413046905177
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-18 04:44:32 +0300

    Reward System

--- src/Controller/Admin/DynamicRoutesController.php
+++ src/Controller/Admin/DynamicRoutesController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Controller\AppController;
+use \Cake\Filesystem\Folder;
+use \Cake\Filesystem\File;
 
 /**
  * Dynamic Routes Controller
@@ -16,9 +18,19 @@ class DynamicRoutesController extends AppController
 
     public function index()
     {
+        // $frontEndContrllers = $this->getControllers();
+        // $frontEndContrllersActions = $this->getActions(null, $frontEndContrllers);
+        // $userContrllers = $this->getControllers('User');
+        // $userContrllersActions = $this->getActions('User', $userContrllers);
+        // $counselorContrllers = $this->getControllers('Counselor');
+        // $counselorContrllersActions = $this->getActions('Counselor', $counselorContrllers);
+
+        // debug($frontEndContrllersActions);
+        // debug($userContrllersActions);
+        // debug($counselorContrllersActions);
         $conditions = $this->_filter_params();
 
-        $dynamicRoutes = $this->paginate($this->DynamicRoutes, ['conditions' => $conditions, 'order'=>['continent'=>'ASC']]);
+        $dynamicRoutes = $this->paginate($this->DynamicRoutes, ['conditions' => $conditions, 'order' => ['continent' => 'ASC']]);
         $parameters = $this->request->getAttribute('params');
         $this->set(compact('dynamicRoutes', 'parameters'));
     }
@@ -27,7 +39,7 @@ class DynamicRoutesController extends AppController
         $conditions = $this->_filter_params();
         $dynamicRoutes = $this->paginate($this->DynamicRoutes, ['conditions' => $conditions]);
         $parameters = $this->request->getAttribute('params');
-        
+
         $this->set(compact('dynamicRoutes', 'parameters'));
     }
 
@@ -43,20 +55,70 @@ class DynamicRoutesController extends AppController
             }
             $this->Flash->error(__('The Dynamic Route could not be saved. Please, try again.'));
         }
-        
+
 
         $this->__common();
-        
+
         $this->set(compact('dynamicRoute'));
+    }
+
+    public function getControllers($prefix = null)
+    {
+        $folder = new Folder(ROOT);
+        $path = "src" . DS . "Controller";
+        if ($prefix)
+            $path .= DS . ucfirst($prefix);
+        $controllerFiles = $folder->cd($path);
+        $controllerFiles = $folder->find(".*.php", true);
+
+        $skipControllers = ['AppController.php', 'AppControllerNoCache.php'];
+        $cleanControllers = [];
+        foreach ($controllerFiles as $key => $controller) {
+
+            if (!in_array($controller, $skipControllers) && strpos($controller, '_') === false) {
+                $cleanControllers[] = str_replace('Controller.php', '', $controller);
+                //Could build a menu based on the controller here now, but do not
+                //have time to do this now.
+            }
+        }
+        return $cleanControllers;
+    }
+    public function getActions($prefix = null, $controllers)
+    {
+        $path = "templates";
+        if ($prefix)
+            $path .= DS . ucfirst($prefix);
+
+        $cleanActions = [];
+        foreach ($controllers as $controller) {
+            if (in_array($controller, ['TempFiles', 'Localizations', 'Error']))
+                continue;
+
+            $fullpath = $path . DS . $controller;
+            $folder = new Folder(ROOT);
+            $actionFiles = $folder->cd($fullpath);
+            $actionFiles = $folder->find(".*.php", true);
+
+            $skipActions = ['import.php','security.php'];
+            $cleanAction = [];
+            foreach ($actionFiles as $key => $action) {
+
+                // debug($controller . '.' . $action);
+                if (!in_array($action, $skipActions) && strpos($action, 'copy') === false && strpos($action, 'old') === false) {
+                    $cleanActions[$controller][] = str_replace('_', '-', str_replace('.php', '', $action));
+                }
+            }
+        }
+        return $cleanActions;
     }
 
     public function edit($id = null)
     {
         $dynamicRoute = $this->DynamicRoutes->get($id);
         if ($this->request->is(['patch', 'post', 'put'])) {
-           $dynamicRoute = $this->DynamicRoutes->patchEntity($dynamicRoute, $this->request->getData());
+            $dynamicRoute = $this->DynamicRoutes->patchEntity($dynamicRoute, $this->request->getData());
 
-            
+
             if ($this->DynamicRoutes->save($dynamicRoute)) {
                 $this->Flash->success(__('The Dynamic Route has been saved.'));
 
@@ -67,7 +129,7 @@ class DynamicRoutesController extends AppController
 
         $this->__common();
 
-        
+
         $this->set(compact('dynamicRoute', 'id'));
         $this->render('add');
     }
