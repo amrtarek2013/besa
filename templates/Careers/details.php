9ceb4f0ec75acdcf459950932d8ee0c103569eb7
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-20 22:44:35 +0300

    Reward System

--- templates/Careers/details.php
+++ templates/Careers/details.php
@@ -24,7 +24,7 @@
 
 </section>
 
-<div class="title-header-blue">
+<div class="title-header-blue" style="padding-bottom:5px;">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
