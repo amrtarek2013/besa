d85eeadd658ab825cd2751aa843ead91ed6fc187
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-20 14:38:57 +0300

    Reward System

--- templates/Careers/details.php
+++ templates/Careers/details.php
@@ -17,7 +17,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="title" style="font-size: 35px;"><?= $career['title'] ?></h1>
+                <h1 class="title" style="font-size: 35px; color:#fff"><?= $career['title'] ?></h1>
 
                 <div class="gridTabes">
                     <a class="btn clear-blue foundation" href="<?= Cake\Routing\Router::url('/' . $g_dynamic_routes['pages.careerapply'] . '/' .  $career['id'] . '/' . $career['title']) ?>">Apply Now</a>
