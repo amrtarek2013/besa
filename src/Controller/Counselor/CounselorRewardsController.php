c05d9bdc023a9ed3f98e02ef1ccded6caa5f611e
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-20 22:17:04 +0300

    Reward System

--- src/Controller/Counselor/CounselorRewardsController.php
+++ src/Controller/Counselor/CounselorRewardsController.php
@@ -21,7 +21,7 @@ class CounselorRewardsController extends AppController
         $conditions = $this->_filter_params();
 
         $user = $this->Auth->user();
-        $conditions['counselor_id'] = $user['id'];
+        $conditions['CounselorRewards.counselor_id'] = $user['id'];
         $counselorRewards = $this->paginate($this->CounselorRewards, ['contain' => ['Users' => ['fields' => ['first_name']]], 'conditions' => $conditions]);
         $parameters = $this->request->getAttribute('params');
         $this->set(compact('counselorRewards', 'parameters'));
