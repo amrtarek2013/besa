d1058f9bcb8ef2e3962fcf0476ec3017a1c7498f
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-20 04:33:07 +0300

    Reward System

--- templates/Admin/CounselorInvoices/index.php
+++ templates/Admin/CounselorInvoices/index.php
@@ -56,7 +56,7 @@
 
                         $actions = [
                             'view'=>$this->Html->link(__('View'), ['action' => 'view', '%id%'], array('class' => 'btn btn-primary btn-sm','icon'=>'fas fa-binoculars')),
-                            'edit' => $this->Html->link(__('Edit'), array('action' => 'edit', '%id%'), array('class' => 'btn btn-info btn-sm', 'icon' => 'fas fa-pencil-alt')),
+                            // 'edit' => $this->Html->link(__('Edit'), array('action' => 'edit', '%id%'), array('class' => 'btn btn-info btn-sm', 'icon' => 'fas fa-pencil-alt')),
 
                             'delete' => $this->Html->link(
 
