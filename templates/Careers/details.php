5a10d2a6f90564c98a477c4d93e23fb28cc6a8cb
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-20 22:33:31 +0300

    Reward System

--- templates/Careers/details.php
+++ templates/Careers/details.php
@@ -17,12 +17,31 @@
                 <img src="<?= $career['image_path'] ?>" alt="" style="width: 100%;">
             </div>
             <!-- <div class="col-md-12" style="padding:30px">
-                <h1 class="title" style="font-size: 35px;"><?= $career['title'] ?></h1>
+                <h1 class="title" style="font-size: 35px;">Location<?= $career['title'] ?></h1>
             </div> -->
         </div>
     </div>
 
 </section>
+
+<div class="title-header-blue" style="background-color: var(--bs-Grey);">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>&nbsp;</p>
+
+                <h1 class="title" style="font-size: 35px;"><?= $career['title'] ?></h1>
+
+                <p><span style="font-size:20px;"><span style="color:#ffffff;">Location: <?= $career['country'] ?> - <?= $career['state'] ?></span></span></p>
+
+                <div class="content">
+                    <p>&nbsp;</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?= $career['text'] ?>
 <?= $career['requirments'] ?>
