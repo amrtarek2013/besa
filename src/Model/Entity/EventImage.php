61e34cdce012637055ce1aed7b6030f08b1ab7e0
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-21 15:59:08 +0300

    Reward System

--- src/Model/Entity/EventImage.php
+++ src/Model/Entity/EventImage.php
@@ -36,7 +36,7 @@ class EventImage extends Entity
     protected function _getImagePath()
     {
 
-        $no_image_path = DS . 'img' . DS . 'photoGalley (1).png';
+        $no_image_path = DS . 'img' . DS . '330x180.png';
         if (!empty($this->image)) {
             $image_path = 'uploads' . DS . 'event_images' . DS . str_replace(DS, "", $this->image);
             if (file_exists(WWW_ROOT . $image_path))
