44ceb766b50444529af09a4d664de8c7ff7ca564
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-09-22 16:16:42 +0300

    Cache menus/snippets/courses/permissions/...

--- templates/Events/event_details.php
+++ templates/Events/event_details.php
@@ -89,6 +89,102 @@
 <?php
 if (!empty($event['left_text']) && $event['id'] == 7) {
     echo $event['left_text'];
+?>
+
+    <!-- <div class="sec-upcoming">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h4>Upcoming</h4>
+
+                    <h5>BESA Study Abroad Expo in Egypt</h5>
+
+                    <p>Engage with university from diverse countries to learn more about study abroad inquiries, including admission requirements, partial scholarships, and registration steps from A to Z!</p>
+
+                    <p>Entry is free! Join us and pave the way for your international education!</p>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="image"><img alt="" loading="lazy" src="/img/Expo.png" /></div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <div class="sec-upcoming">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="content">
+                        <div class="grid-3col">
+                            <div class="box">
+                                <h3><span style="font-size:28px;"><strong>Cairo&nbsp;</strong></span><br />
+                                    <br />
+                                    Where
+                                </h3>
+
+                                <p>Dusit Thani Lakeview, El-Tessen street, New Cairo,</p>
+                            </div>
+
+                            <div class="box">
+                                <h3><br />
+                                    <br />
+                                    When
+                                </h3>
+
+                                <p>28th September&nbsp;2023:&nbsp;10:30 AM- 01:30 PM, 4:00 PM - 8:00 PM<br />
+                                    29th September 2023:&nbsp;4:00 PM - 8:00 PM</p>
+                            </div>
+
+                            <div class="box">
+                                <h3><br />
+                                    <br />
+                                    Attending countries
+                                </h3>
+
+                                <div class="country d-flex"><img alt="" src="/webroot/filebrowser/upload/images/Untitled%20%2871%20x%2063%20px%29%20%281%29.png" style="width: 71px; height: 63px;" /> <img alt="" src="/webroot/filebrowser/upload/images/Untitled%20%2871%20x%2063%20px%29%20%282%29.png" style="width: 71px; height: 63px;" /> <img alt="" src="/webroot/filebrowser/upload/images/Untitled%20%2871%20x%2063%20px%29%20%283%29.png" style="width: 71px; height: 63px;" /> <img alt="" src="/img/hungary_glossy_round_icon_256.png" /> <img alt="" src="/img/canada_glossy_round_icon_640.png" /></div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="btn btn-register" href="https://besaeg.com/education-fair/ief-form?location=cairo">Register Now</a>
+
+                    <div class="content">
+                        <div class="grid-3col">
+                            <div class="box">
+                                <h3><span style="font-size:28px;"><strong>Alexandria&nbsp;</strong></span><br />
+                                    <br />
+                                    Where
+                                </h3>
+
+                                <p>Hilton Corniche,&nbsp;El Geish Road, Sidi Bishr, Alexandria</p>
+                            </div>
+
+                            <div class="box">
+                                <h3><br />
+                                    <br />
+                                    When
+                                </h3>
+
+                                <p>October 1st&nbsp;2023:&nbsp;10:00 AM- 01:30 PM, 4:00 PM - 8:00 PM</p>
+                            </div>
+
+                            <div class="box">
+                                <h3><br />
+                                    <br />
+                                    Attending countries
+                                </h3>
+
+                                <div class="country d-flex"><img alt="" src="/webroot/filebrowser/upload/images/Untitled%20%2871%20x%2063%20px%29%20%281%29.png" style="width: 71px; height: 63px;" /> <img alt="" src="/webroot/filebrowser/upload/images/Untitled%20%2871%20x%2063%20px%29%20%282%29.png" style="width: 71px; height: 63px;" /> <img alt="" src="/webroot/filebrowser/upload/images/Untitled%20%2871%20x%2063%20px%29%20%283%29.png" style="width: 71px; height: 63px;" /> <img alt="" src="/img/hungary_glossy_round_icon_256.png" /> <img alt="" src="/img/canada_glossy_round_icon_640.png" /></div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="btn btn-register" href="https://besaeg.com/education-fair/ief-form?location=alexandria">Register Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php
     echo $this->element('event-slider', ['event_images' => $event['event_images']]);
 }
 ?>
