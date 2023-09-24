2b79abfa04593a3bed8f76a3cd735828ffffca4a
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-09-24 12:37:47 +0300

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Events/event_details.php
+++ templates/Events/event_details.php
@@ -104,7 +104,7 @@ if (!empty($event['left_text']) && $event['id'] == 7 && !empty($event['fair_even
                     <div class="col-md-12">
                         <?php foreach ($event['fair_events'] as $fairEvent) { ?>
                             <div class="content">
-                                <h3><span style="font-size:28px;"><strong><?= $fairEvent['title'] ?></strong></span></h3>
+                                    <h3 style="font-family: 'Poppins'; font-size: 24px; font-style: normal;font-weight: 400;line-height: 35px; " ><strong><?= $fairEvent['title'] ?></strong></h3>
                                 <div class="grid-3col">
                                     <div class="box">
                                         <h3>
