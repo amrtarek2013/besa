4b8a436400c6b8546e482e127592df94e9aff855
DrKhaledAbdo <khaledabdo2012@gmail.com>
2023-10-05 13:31:11 +0300

    Career Pdf new page

--- templates/Careers/details.php
+++ templates/Careers/details.php
@@ -8,43 +8,47 @@
         line-height: 32px;
     }
 </style>
-<section class="main-banner  inner-serv unitedKingdom-banner">
-    <div class="container-fluid">
-        <div class="row">
-
-            <div class="col-md-12">
-
-                <img src="<?= $career['image_path'] ?>" alt="" style="width: 100%;">
-            </div>
-            <!-- <div class="col-md-12" style="padding:30px">
-                <h1 class="title" style="font-size: 35px;">Location<?= $career['title'] ?></h1>
-            </div> -->
-        </div>
-    </div>
-
-</section>
-
-<div class="title-header-blue" style="padding-bottom:0px;">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="title" style="font-size: 35px; text-align:center; color:var(--bs-white)"><?= $career['title'] ?> <h1>
-                        <h3 class="title" style="font-size: 25px; text-align:center; color:var(--bs-white)"> <?= $career['country'] ?> - <?= $career['state'] ?></h3>
-                        <?php
-
-                        if (!isset($show_pdf)) {
-                        ?>
-                            <h3 style="text-align: center;">
-                                <a href="<?= Cake\Routing\Router::Url('/career-details/' . $permalink . '/' . $id . '/1') ?>" target="_blank" style="color: var(--bs-main);"><i class="fa-solid fa-file-pdf"></i> View Job Full Requirments</a>
-                            </h3>
-                        <?php } ?>
-
-            </div>
-        </div>
-    </div>
-</div>
 <?php
 
+if (false) {
+?>
+    <section class="main-banner  inner-serv unitedKingdom-banner">
+        <div class="container-fluid">
+            <div class="row">
+
+                <div class="col-md-12">
+
+                    <img src="<?= $career['image_path'] ?>" alt="" style="width: 100%;">
+                </div>
+                <!-- <div class="col-md-12" style="padding:30px">
+                <h1 class="title" style="font-size: 35px;">Location<?= $career['title'] ?></h1>
+            </div> -->
+            </div>
+        </div>
+
+    </section>
+
+    <div class="title-header-blue" style="padding-bottom:0px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="title" style="font-size: 35px; text-align:center; color:var(--bs-white)"><?= $career['title'] ?> <h1>
+                            <h3 class="title" style="font-size: 25px; text-align:center; color:var(--bs-white)"> <?= $career['country'] ?> - <?= $career['state'] ?></h3>
+                            <?php
+
+                            if (!isset($show_pdf)) {
+                            ?>
+                                <h3 style="text-align: center;">
+                                    <a href="<?= Cake\Routing\Router::Url('/career-details/' . $permalink . '/' . $id . '/1') ?>" target="_blank" style="color: var(--bs-main);"><i class="fa-solid fa-file-pdf"></i> View Job Full Requirments</a>
+                                </h3>
+                            <?php } ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+<?php
+}
 if (isset($show_pdf)) {
 ?>
     <embed src="<?= $career['file_path'] ?>#toolbar=0&navpanes=0&scrollbar=0" type="application/pdf" width="100%" height="800px" />
@@ -52,18 +56,19 @@ if (isset($show_pdf)) {
 <?php } else { ?>
     <?= $career['text'] ?>
     <?= $career['requirments'] ?>
+<?php } ?>
 
-    <section class=" tabes british-tabes">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
 
-                    <div class="gridTabes">
-                        <a class="btn clear-blue foundation" href="<?= Cake\Routing\Router::url('/' . $g_dynamic_routes['pages.careerapply'] . '/' .  $career['id'] . '/' . $career['title']) ?>">Apply Now</a>
+<section class=" tabes british-tabes">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
 
-                    </div>
+                <div class="gridTabes">
+                    <a class="btn clear-blue foundation" href="<?= Cake\Routing\Router::url('/' . $g_dynamic_routes['pages.careerapply'] . '/' .  $career['id'] . '/' . $career['title']) ?>">Apply Now</a>
+
                 </div>
             </div>
         </div>
-    </section>
-<?php } ?>
+    </div>
+</section>
