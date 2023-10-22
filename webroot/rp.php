4baddf921480faa9ef0148bf0e22cbf075c7f73a
KG <developerkg@thesitefactory.com.au>
2023-10-22 10:49:56 +0300

    testing git

--- webroot/rp.php
+++ webroot/rp.php
@@ -1,5 +1,5 @@
 <?php 
-exec("cd /home/eclbess7ogbr/public_html/besaKG && git pull origin master", $out);
+exec("cd /home/eclbess7ogbr/public_html/besaKG && git pull --no-ff", $out);
 echo "<pre>";
 print_r($out);
 echo "</pre>";
