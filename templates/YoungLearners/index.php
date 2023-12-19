6cee01cfd34ddc4f40f9f20d6f0ef7d9e527293a
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-12-19 13:34:04 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/YoungLearners/index.php
+++ templates/YoungLearners/index.php
@@ -12,25 +12,5 @@
 
 </div>
 
-<section class="main-banner banner-about-us university-Placement-banner">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-6" style="position: relative;">
-                <div class="relative-box-about ">
-                    <h1 class="relative-text">v</h1>
-                    <h2 class="title text-left">YOUNG <br> LEARNERS</h2>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="background-banner-color">
-                    <img src="<?= WEBSITE_URL ?>img/hero-bg44.png" alt="" width="">
-                    <img src="<?= WEBSITE_URL ?>img/dots-153.png" width="" alt="" class="relative-dots-about">
-                </div>
-            </div>
-
-        </div>
-    </div>
-
-    <?= $this->element('placement_list', ['placementList' => $youngLearners]); ?>
-</section>
+<?= $this->element('placement_list', ['placementList' => $youngLearners]); ?>
 <?= $book_free_meeting ?>
--- templates/element/placement_list.php
+++ templates/element/placement_list.php
@@ -2,33 +2,24 @@
 
 // print_r($placementList);
 if (!empty($placementList)) : ?>
-    <?php foreach ($placementList as $placementItem) :
-    ?>
-        <div class="FoundationProgramFAQs">
-            <div class="itemFAQs">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="box-item display-flex">
-                                <img src="<?= $placementItem['image_path'] ?>" alt="<?= $placementItem['title'] ?>" loading="lazy">
-                                <div class="content-blog">
-                                    <p><?= $placementItem['title'] ?></p>
-                                    <?= $placementItem['short_text'] ?>
-                                    <!-- <ul>
-                                        <li>What is pre sessional English?</li>
-                                        <li>What is the entry requirement for pre-sessional English?</li>
-                                        <li>How will a pre-sessional English course support my further studies?</li>
-                                        <li>Which countries offer pre-sessional English?</li>
-                                        <li>How long is a pre-sessional English course?</li>
-                                    </ul> -->
-                                </div>
-                            </div>
-                        </div>
+    <div class="FoundationProgramFAQs">
+        <?php foreach ($placementList as $placementItem) :
+        ?>
+                <div class="itemFAQs">
+                    <div class="box-item display-flex">
+                        <img src="<?= $placementItem['image_path'] ?>" alt="<?= $placementItem['title'] ?>" loading="lazy">
+                        <div class="content-blog">
+                            <p><?= $placementItem['title'] ?></p>
+                            <?= $placementItem['short_text'] ?>
+                        </div>       
                     </div>
                 </div>
-            </div>
 
+
+        <?php endforeach; ?>
+        
         </div>
 
-    <?php endforeach; ?>
+    </div>
+  
 <?php endif; ?>
