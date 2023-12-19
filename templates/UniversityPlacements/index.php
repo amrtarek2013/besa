8c409e72f45118b003e7ee0171c9173d114f3b07
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-12-19 14:52:08 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/UniversityPlacements/index.php
+++ templates/UniversityPlacements/index.php
@@ -1,22 +1,16 @@
-<section class="main-banner banner-about-us university-Placement-banner">
+<div class="hero-section hero-placement">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-6" style="position: relative;">
-                <div class="relative-box-about ">
-                    <h1 class="relative-text">University <br> Placement</h1>
-                    <h2 class="title text-left">University <br> Placement</h2>
-                </div>
+        <div class="col-md-6">
+            <div class="img-hero">
+                <img src="<?= WEBSITE_URL ?>img/new-desgin/hero-learners.png" alt="hero Young learners">
             </div>
-            <div class="col-md-6">
-                <div class="background-banner-color">
-                    <img src="<?=WEBSITE_URL?>img/hero-bg44.png" alt="" width="">
-                    <img src="<?=WEBSITE_URL?>img/dots-153.png" width="" alt="" class="relative-dots-about">
-                </div>
-            </div>
-
+        </div>
+        <div class="col-md-6">
+            <h1 class="title-hero">Young Learners </h1>
         </div>
     </div>
+</div>
 
-    <?=$this->element('placement_list', ['placementList' => $universityPlacements]); ?>
-</section>
+<?=$this->element('placement_list', ['placementList' => $universityPlacements]); ?>
+
 <?= $book_free_meeting ?>
