507885b549917a43506038b3b2ae76ae36c19e10
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-12-19 13:06:31 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Services/index.php
+++ templates/Services/index.php
@@ -31,11 +31,7 @@
 </section> -->
 
 <section class="services-rendered">
-    <!-- <div class="line-ellipse line-ellipse3col">
-        <span></span>
-        <span></span>
-        <span></span>
-    </div> -->
+   
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -84,6 +80,7 @@
     </div>
 
 </section>
+
 
 
 <?= $book_free_meeting ?>
--- templates/YoungLearners/index.php
+++ templates/YoungLearners/index.php
@@ -1,9 +1,23 @@
+<div class="hero-section hero-learners">
+    <div class="container-fluid">
+        <div class="col-md-6">
+            <div class="img-hero">
+                <img src="<?= WEBSITE_URL ?>img/new-desgin/hero-learners.png" alt="hero Young learners">
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h1 class="title-hero">Young Learners </h1>
+        </div>
+    </div>
+
+</div>
+
 <section class="main-banner banner-about-us university-Placement-banner">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6" style="position: relative;">
                 <div class="relative-box-about ">
-                    <h1 class="relative-text">YOUNG <br> LEARNERS</h1>
+                    <h1 class="relative-text">v</h1>
                     <h2 class="title text-left">YOUNG <br> LEARNERS</h2>
                 </div>
             </div>
