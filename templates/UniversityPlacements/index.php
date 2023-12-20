6cf94e3c9cfcfc250783c32c561954e6996054fe
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-12-20 09:28:50 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/UniversityPlacements/index.php
+++ templates/UniversityPlacements/index.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="col-md-6">
             <div class="img-hero">
-                <img src="<?= WEBSITE_URL ?>img/new-desgin/hero-learners.png" alt="hero Young learners">
+                <img src="<?= WEBSITE_URL ?>img/new-desgin/hero-Placements.png" alt="hero Young learners">
             </div>
         </div>
         <div class="col-md-6">
-            <h1 class="title-hero">Young Learners </h1>
+            <h1 class="title-hero">University <span>Placements</span> </h1>
         </div>
     </div>
 </div>
