bd5c82c7d721fb086fc678b7a79b7d3c020d5652
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2023-12-27 14:25:46 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Events/event_details.php
+++ templates/Events/event_details.php
@@ -8,67 +8,35 @@
 use Cake\Routing\Router;
 
 ?>
-<section class="main-banner british-banner fair-banner <?= $event['style'] ?>" style="padding-bottom:0 !important;">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <div class="background-banner-color">
-                    <img src="<?= $event['main_image_path'] ?>" alt="" style="z-index: 2;" width="">
-                    <img src="<?= WEBSITE_URL ?>img/dots-153.png" width="" alt="" class="relative-dots-about">
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="relative-box-about ">
 
-                    <?php $tt = explode(' ', trim($event['title'])) ?>
-                    <h1 class="relative-text"><?= substr($tt[0], 0, 6) ?></h1>
-                    <h2 class="title text-left"><?= $event['title'] ?></h2>
-                    <?php
-                    /*if (!isset($event['style'])) {
-                    ?>
-                        <p class="relative-textP">
-                            <?= $event['text'] ?>
-                        </p>
-                    <?php } else { ?>
-
-                        <?= $event['text'] ?>
-                    <?php }*/ ?>
-
-                </div>
+<div class="hero-section hero-education-fair">
+    <div class="container-fluid">
+        <div class="col-md-6">
+            <div class="img-hero">
+                <img src="<?= WEBSITE_URL ?>img/new-desgin/hero-education-fair.png" alt="hero about us">
             </div>
         </div>
-
-        <?php
-        if (isset($event['style']) && $event['style'] == 1) {
-        ?>
-
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="text text-radius" style="<?= !empty($event['font_color']) ? 'color:' . $event['font_color'] : '' ?><?= !empty($event['background_color']) ? ';background-color:' . $event['background_color'] : '' ?>">
-                        <p class="descrp">
-                            <?= $event['center_text'] ?>
-                        </p>
-
-                    </div>
-                </div>
-
-
-            </div>
-
+        <div class="col-md-6">
+            <h1 class="title-hero">International <span>Education Fair</span> </h1>
+        </div>
     </div>
 
-<?php } else { ?>
-    </div>
-    <div class="container-fluid">
+</div>
+
+<section class="bottom-hero-section bottom-education-fair ">
+    <div class="container">
         <div class="row">
-            <div class="col-md-12" style="padding: 0;">
-                <div class="title-banner-blue " style="<?= !empty($event['font_color']) ? 'color:' . $event['font_color'] : '' ?><?= !empty($event['background_color']) ? ';background-color:' . $event['background_color'] : '' ?>">
+            <div class="col-md-12" style="">
+                <div class="title-bottom-hero">
                     <?= $event['center_text'] ?>
                 </div>
             </div>
         </div>
     </div>
-<?php } ?>
+</section>
+
+
+
 
 <?php
 /*if (isset($event['style'])) {
