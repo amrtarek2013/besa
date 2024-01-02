8e1b4ddcc6266345ef9604edd1a2245f225ad352
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2024-01-02 17:45:45 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Blogs/index.php
+++ templates/Blogs/index.php
@@ -13,48 +13,35 @@
 </div>
 
 <div class="blogs">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                <?php
-                    if (!empty($blogs)) : ?>
-                        <?php $counter = 0; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php if (!empty($blogs)) : ?>
                     <div class="grid-blogs-container">
-                    <?php foreach ($blogs as $blog) : ?>
-                                <?php $counter++; ?>
-                                <div class="box-blog display-flex">
-                                    <img src="<?= $blog['image_path'] ?>" alt="This Is Blog Img 1" loading="lazy">
-                                    <div class="content-blog">
-                                        <h4><?= $blog['title'] ?></h4>
-                                        <a href="<?= Cake\Routing\Router::url('/' . $g_dynamic_routes['blogs.details'] . '/' . $blog['permalink']) ?>" class="btn btn-secondary read-anchor">READ MORE</a>
-                                    </div>
+                        <?php foreach ($blogs as $blog) : ?>
+                            <div class="box-blog display-flex">
+                                <img src="<?= $blog['image_path'] ?>" alt="This Is Blog Img 1" loading="lazy">
+                                <div class="content-blog">
+                                    <h4><?= $blog['title'] ?></h4>
+                                    <a href="<?= Cake\Routing\Router::url('/' . $g_dynamic_routes['blogs.details'] . '/' . $blog['permalink']) ?>" class="btn btn-secondary read-anchor">READ MORE</a>
                                 </div>
-
-                               
-
-                            <?php endforeach; ?>
-
-                           
-                    </div>
-
-                    
-
-                     
-
-                        <?php if (sizeof($universities) == 20) : ?>
-                            <div class="paginator">
-                                <ul class="pagination">
-                                    <?= $this->Paginator->first(' << ') ?>
-                                    <?= $this->Paginator->prev(' < ') ?>
-                                    <?= $this->Paginator->numbers() ?>
-                                    <?= $this->Paginator->next(' > ') ?>
-                                    <?= $this->Paginator->last(' >> ') ?>
-                                </ul>
-                                <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
                             </div>
-                        <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                    <?php if (sizeof($universities) == 20) : ?>
+                        <div class="paginator">
+                            <ul class="pagination">
+                                <?= $this->Paginator->first(' << ') ?>
+                                <?= $this->Paginator->prev(' < ') ?>
+                                <?= $this->Paginator->numbers() ?>
+                                <?= $this->Paginator->next(' > ') ?>
+                                <?= $this->Paginator->last(' >> ') ?>
+                            </ul>
+                            <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
+                        </div>
                     <?php endif; ?>
-                </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
+</div>
