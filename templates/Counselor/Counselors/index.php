1c7b954a916ce1c46c97ce59b76f62dec55bc6ef
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2024-01-09 11:05:42 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Counselor/Counselors/index.php
+++ templates/Counselor/Counselors/index.php
@@ -2,37 +2,45 @@
 
 use Cake\Routing\Router;
 ?>
-<section class="main-banner register-banner Create-account-banner">
 
+
+<div class="hero-section hero-counselors">
+    <div class="container-fluid">
+        <div class="col-md-6">
+            <div class="img-hero icon-pencil-note">
+                <img src="<?= WEBSITE_URL ?>img/new-desgin/hero-counselors.png" alt="hero Counselors Portal">
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h1 class="title-hero">School <span>Counselors Portal</span> </h1>
+        </div>
+    </div>
+</div>
+
+<section class="bottom-hero-section ">
     <div class="container">
         <div class="row">
-            <div class="col-md-7">
-                <div class="background-banner-color">
-                    <img src="<?= WEBSITE_URL ?>img/school_councelor_portal_bg.png" alt="" style="z-index: 2;"  width="">
-                    <img src="<?= WEBSITE_URL ?>img/dots-153.png" width="" alt="" class="relative-dots-about">
+            <div class="col-md-12" style="">
+                <div class="title-bottom-hero">
+                    <h2 class="title">Welcome to <br>the School Counselor Portal</h2>
+                    <p class="bold-descrp">Register and Unlock Rewards for Your Students' Success</p>
                 </div>
             </div>
-            <div class="col-md-5">
-                <div class="relative-box-about ">
-                    <h1 class="relative-text">Portal</h1>
-                    <h2 class="title text-left">School Counselors Portal</h2>
-                </div>
-            </div>
-
-
-            <div class="col-md-8 mr">
-                <!-- <h4 class="title">Create an account to apply</h4>
-
-                <p class="light-para">For the purpose of applying regulation, your details are required.</p> -->
-                <?= $school_counselors_portal ?>
-
-
-            </div>
-            <div class="col-md-4 ml">
+        </div>
+    </div>
+</section>
+<div class="section-content-and-form">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="d-flex">
+                    <div class="content content-text">
+                        <?= $school_counselors_portal ?>
+                    </div>
                 <!-- <div class="container-formBox blue-border ">
                     <form action="/counselor/login" class="login" method="post">
 
-                        <?= $this->Form->create($counselor, array('url' => '/counselor/login', 'id' => 'FormLogin', 'class' => 'login')); ?>
+                        <?= $this->Form->create($counselor, array('url' => '/counselor/login', 'id' => 'FormLogin', 'class' => 'login ')); ?>
                         <h4 class="title">Log in</h4>
                         <div class="grid-container">
                             <div class="form-area">
@@ -44,18 +52,18 @@ use Cake\Routing\Router;
                                 <input type="password" id="password" name="password" placeholder="password">
                             </div>
                             <div class="form-area">
-                <button type="submit" class="btn clear-blue">LOG IN</button>
-            </div>
-        </div>
-        </form>
-    </div>
-    <br /><br /> -->
-                <div class="container-formBox blue-border ">
+                                    <button type="submit" class="btn clear-blue">LOG IN</button>
+                                </div>
+                            </div>
+                            </form>
+                        </div>
+                        <br /><br /> -->
+                <div class="container-formBox  sideform ">
                     <!-- <form action="/counselor/register" class="login" method="post"> -->
 
                     <?= $this->Form->create($counselor, array('url' => '/counselor/register', 'id' => 'FormRegister', 'class' => 'login')); ?>
-                    <h5 class="title">Together, let's create a brighter future for your students!</h5>
-                    <p class="light-para">For the purpose of applying regulation, your details are required.</p>
+                    <h5 class="title-form">Together, let's create a brighter future for your students!</h5>
+                    <p class="white-para">For the purpose of applying regulation, your details are required.</p>
                     <div class="grid-container">
                         <?= $this->Form->control('name', [
                             'placeholder' => 'Full Name', 'label' => 'Full Name*', 'required' => true,
@@ -85,14 +93,32 @@ use Cake\Routing\Router;
                         <?= $this->element('security_code') ?>
 
                         <div class="form-area">
-                            <a href="<?= Router::url('/counselor/login') ?>" class="forgot-link">Login</a>
+                            <!-- <a href="<?php// Router::url('/counselor/login') ?>" class="forgot-link">Login</a> -->
                             <br />
                             <button type="submit" class="btn clear-blue">SUBMIT</button>
                         </div>
                     </div>
                     <!-- </form> -->
                 </div>
+                </div>
             </div>
+        </div>
+    </div>
+</div>
+
+<section class="main-banner register-banner Create-account-banner">
+
+    <div class="container">
+        <div class="row">
+           
+
+            <div class="col-md-6 mr">
+   
+                
+
+
+            </div>
+          
         </div>
     </div>
 </section>
