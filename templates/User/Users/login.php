4052e43958cc8828979c343a5db09e1a367c28b2
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2024-01-15 10:34:27 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/User/Users/login.php
+++ templates/User/Users/login.php
@@ -41,3 +41,62 @@
   </div>
 </div>
 
+
+
+<section class="main-banner register-banner">
+
+  <div class="container">
+
+    <div class="row">
+      <?= $this->Form->create(null, array('id' => 'FormLogin')); ?>
+      <div class="col-md-2">
+
+        &nbsp;
+      </div>
+      <div class="col-md-8">
+        <div class="container-formBox">
+          <h4 class="title">Login Details</h4>
+          <div class="grid-container" style="display: block;">
+
+
+            <?= $this->Form->control('email', [
+              'placeholder' => 'Email', 'class' => 'form-control', 'label' => 'Email*', 'required' => true,
+              'templates' => ['inputContainer' => '<div class="form-area {{rquired}}">{{content}}</div>']
+            ]) ?>
+
+
+          </div>
+          <br /><br />
+          <div class="grid-container" style="display: block;">
+
+            <?= $this->Form->control('password', [
+              'type' => 'password',
+              'placeholder' => 'Password',
+              'class' => 'form-area',
+              'value' => '',
+              'autocomplete' => false,
+              'label' => 'Password*',
+              'templates' => ['inputContainer' => '<div class="form-area {{required}}">{{content}}<i class="toggle-password fas fa-eye" onclick="togglePasswordVisibility(\'password\')"></i></div>']
+            ]) ?>
+
+          </div>
+          <div class="container-submit login-box-links" style="padding: 0 20px;">
+            <div class="checkboxes links">
+              <div>
+
+                <a href="<?= $this->Url->Build('/user/register') ?>"> Register?</a>
+              </div>
+              <div>
+
+                <a href="<?= $this->Url->Build('/user/forgot-password') ?>"> Forget Password?</a>
+              </div>
+            </div>
+
+            <button type="submit" class="btn clear-blue">Sign In</button>
+          </div>
+        </div>
+      </div>
+      <?= $this->Form->end() ?>
+    </div>
+  </div>
+</section>
