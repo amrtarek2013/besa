5432da643a70ecc428ebce5899ba323c77e29385
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2024-01-15 22:52:58 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Pages/subject_discovery.php
+++ templates/Pages/subject_discovery.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="col-md-6">
             <div class="img-hero">
-                <img src="<?= WEBSITE_URL ?>img/new-desgin/hero-subject-discovery.png" alt="hero Subject Discovery">
+                <img src="<?= WEBSITE_URL ?>img/new-desgin/hero-subject-discovery.png" alt="hero Subject Discovery" loading="lazy">
             </div>
         </div>
         <div class="col-md-6">
@@ -11,17 +11,18 @@
     </div>
 
 </div>
-<section class="bottom-hero-section ">
+<section class="bottom-hero-section  bottom-hero-section-subject-discovery">
     <div class="container">
         <div class="row">
             <div class="col-md-12" style="">
                 <div class="title-bottom-hero">
                     <h4>  <span  class="bold2"> Explore Your  </span>Academic Journey with the Subject Selection Assessment</h4>
-                    <img src="https://dummyimage.com/1706x874/d9d9d9/000000.png" alt="hero Subject Discovery">
-                    
+                    <img src="https://dummyimage.com/1706x874/d9d9d9/000000.png" alt="hero Subject Discovery" loading="lazy">
                     <p class="description">
-                        is an international organization with branches in egypt, sudan,kuwait and the UKits 
-                        deliciated to is an international organization with branches in egypt, sudan,kuwait and the UK its deliciated to    
+                        Welcome to the Subject Discovery Assessment, 
+                        your gateway to an educational adventure tailored to your unique personality. 
+                        Whether you're a high school student seeking to chart your 
+                        academic path or dreaming of studying abroad, this assessment is your compass    
                     </p>
                 </div>
             </div>
@@ -31,3 +32,102 @@
     </div>
 </section>
 
+
+<div class="discovery-assessment">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title-with-icon">
+                    <img class="sun-image" src="<?= WEBSITE_URL ?>img/new-desgin/ellipse798.svg" alt="" loading="lazy">
+
+                    <h2>What you will get from the Subject <br> Discovery Assessment?</h2>
+                    
+                </div>
+
+                <div class="container-discovery-assessment">
+                   <div class="items-ben">
+                        <div class="benefit">
+                            <img src="<?= WEBSITE_URL ?>img/new-desgin/3d-render-hand-with-light-bulb-creative-idea 1.png"  alt="Personalized Insights" loading="lazy">
+                            <div class="content"> 
+                                <h3>Personalized Insights:</h3>
+                                <ul>
+                                    <li>
+                                        <p>
+                                            We delve into your personality, interests,
+                                            and ambitions to provide a comprehensive understanding of 
+                                            the fields of study that match your insights.
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>    
+                        </div>
+                        <div class="benefit">
+                            <img src="<?= WEBSITE_URL ?>img/new-desgin/8740981.png" alt="Confidence in Choices" loading="lazy">
+                            <div class="content"> 
+                                <h3>Confidence in Choices:</h3>
+                                <ul>
+                                    <li>
+                                        <p>Gain clarity and confidence in selecting the right academic path for high school and beyond.</p>
+                                    </li>
+                                </ul>
+                            </div> 
+                        </div>
+                        <div class="benefit">
+                            <img src="<?= WEBSITE_URL ?>img/new-desgin/7938197.png"  alt="Fulfillment and Success" loading="lazy">
+                            <div class="content"> 
+                                <h3>Fulfillment and Success:</h3>
+                                <ul>
+                                    <li>
+                                        <p>Our assessment helps you go beyond mere education, ensuring your academic 
+                                        journey is deeply fulfilling and sets the stage for future.</p>
+                                    </li>
+                                </ul>
+                            </div> 
+                        </div>
+                    </div>
+                    <div class="image-right">
+                        <img src="<?= WEBSITE_URL ?>img/new-desgin/3d-girl-character-with-question-mark_23-2149086087 1.png" alt="" loading="lazy">
+
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+  
+<div class="study-abroad-new">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="title-new">
+                    <h4 class="title">  <span class="bold2"> Your Path</span> to Studying Abroad For those <br> with dreams of studying abroad</h4>
+
+                </div>
+                 <div class="grid-2col">
+                    <div class="item-study">
+                        <p>understanding your academic interests is the first step towards international adventure</p>
+                    </div>
+                    <div class="item-study">
+                        <p>Begin Your Discovery Journey Empower your educational journey and amplify your study abroad ambitions with the Discovery Assessment</p>
+                    </div>
+                    <div class="row-study">
+                        <div class="item-study">
+                            <p> Selecting the right field of study is pivotal in crafting a meaningful and rewarding experience. </p>
+                        </div>
+                        <div class="item-study">
+                            <p>Uncover your potential, embrace academic excellence, and embark on your adventure today</p>
+                        </div>
+
+                    </div>
+                    <div class="image-study">
+                        <img src="<?= WEBSITE_URL ?>img/new-desgin/education-concept-with-woman-reading_23-2149935200 1.png" alt="" loading="lazy">
+
+                    </div>
+                 </div>
+                 <a href="#" class="btn btn-new">Begin your discovery journey <img src="<?=WEBSITE_URL?>img/new-desgin/arrow-right-white.svg"  alt="" >  </a>
+            </div>
+        </div>
+    </div>
+</div>
