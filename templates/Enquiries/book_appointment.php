c4955c4f62ee4491ea22f952550d1f4cf93c6da8
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2024-01-16 12:28:03 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Enquiries/book_appointment.php
+++ templates/Enquiries/book_appointment.php
@@ -11,32 +11,7 @@
     </div>
 </div>
 
-<section class="bottom-hero-section  bottom-book-appointment">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12" style="">
-                <div class="title-bottom-hero">
-                    <h4>   
-                        </span>If you're looking for <span  class=""> guidance on studying abroad </span> and prefer <br>
-                        to avoid in-person meetings or face challenges such as commuting, we have the perfect solution for you
-                    </h4>
-                    <p class="description">
-                        Get in touch with us to arrange a remote counseling session with Besa's <br> 
-                        experienced education counselors. <br><br>
-                        They will offer comprehensive assistance to help you achieve your dream of <br>
-                        studying abroad. This includes evaluating your study options, selecting the <br>
-                        perfect destination, university, and course, as well as aiding with application <br>  
-                        submissions, visa guidance, accommodation services, and more – all of which <br> 
-                        can be conveniently accessed online. <br><br>
-                        Don't let obstacles hold you back; make the most of our remote counseling <br> services today.   
-                    </p>
-                </div>
-            </div>
-
-
-        </div>
-    </div>
-</section>
+<?= $bookAppointmentSnippet ?>
 
 <section class="main-banner Create-account-banner  visitors-application book-appointment">
     <div class="container-fluid">
@@ -45,10 +20,7 @@
                 <?= $this->Form->create(null, array('url' => 'contact-us', 'type' => 'file', 'id' => 'FormBookAppointment', 'class' => 'register')); ?>
 
                 <input type="hidden" id="type" name="type" value="book-appointment">
-                <p class="light-para">
-                    <?= $bookAppointmentSnippet ?>
-
-                </p>
+               
 
                 <div class="container-formBox">
                     <div class="grid-container">
