3408b1f9d04994ee71cc0a21cbbf2a8d52bd3c3c
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2024-01-22 16:00:44 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/element/navbar.php
+++ templates/element/navbar.php
@@ -296,11 +296,18 @@ use Cake\Routing\Router;
                         </div>
 
                         <?php if (isset($_SESSION['Auth']['User'])) { ?>
+                            <div class="details-user">
+                                    <div class="header-details">
+                                        <img src="" alt="">
+                                        <h4 class="name-user"></h4>
+                                        <i class="fa-solid fa-angle-down"></i>
+                                    </div>
+                                </div>
 
-
-                            <a href="/user/logout" class="btn SecondaryBtn logout">Logout
-                                <img src="<?= WEBSITE_URL ?>img/icon/login.png" alt="">
-                            </a>
+                            <!-- <a href="/user/logout" class="btn SecondaryBtn logout">Logout
+                                <img src="<?php WEBSITE_URL ?>img/icon/login.png" alt="">
+                            </a> -->
+                            
 
                             <a href="/user" class="btn MainBtn">Profile</a>
                         <?php } else if (isset($_SESSION['Auth']['Counselor'])) { ?>
