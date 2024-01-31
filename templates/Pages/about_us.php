2e2cb3dbee74ac5f0f130b0277896751aba5a4a6
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2024-01-31 10:05:06 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- templates/Pages/about_us.php
+++ templates/Pages/about_us.php
@@ -118,11 +118,11 @@
             }
         });
 
-        $(window).resize(function() {
-            var $cols = $(".timeline-horizontal .timeline-dots li");
-            var numberOfCols = $cols.length;
-            $cols.css("width", 100 / numberOfCols + "%");
-        }).resize(); // Trigger resize to set initial width
+        // $(window).resize(function() {
+        //     var $cols = $(".timeline-horizontal .timeline-dots li");
+        //     var numberOfCols = $cols.length;
+        //     $cols.css("width", 100 / numberOfCols + "%");
+        // }).resize(); // Trigger resize to set initial width
 
     });
 </script>
