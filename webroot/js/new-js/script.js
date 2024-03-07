50c88589d6d13e7f0ed1eaeaa36b823d631e21b4
Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>
2024-03-07 17:03:54 +0200

    Signed-off-by: Eslam abselnasser <96479431+imsemoo@users.noreply.github.com>

--- webroot/js/new-js/script.js
+++ webroot/js/new-js/script.js
@@ -355,75 +355,7 @@ $(document).ready(function () {
     })
   );
 
-  
-
-  // // jQuery animated number counter from zero to value
-  // var counterAbout = $(".about-us");
-  // var counterBanner = $(".main-banner");
-  // var winHeight = $(window).height();
-  // if (counterAbout.length) {
-  //   var firEvent = false,
-  //     objectPosTop = $(".about-us").offset().top;
-  //   //when element shows at bottom
-  //   var elementViewInBottom = objectPosTop - winHeight;
-  //   $(window).on("scroll", function () {
-  //     var currentPosition = $(document).scrollTop();
-  //     //when element position starting in viewport
-  //     if (currentPosition > elementViewInBottom && firEvent === false) {
-  //       firEvent = true;
-  //       animationCounter();
-  //     }
-  //   });
-  // } else if (counterBanner.length) {
-  //   var firEvent = false,
-  //     objectPosTop = $(".main-banner").offset().top;
-  //   //when element shows at bottom
-  //   var elementViewInBottom = objectPosTop - winHeight;
-  //   $(window).on("scroll", function () {
-  //     var currentPosition = $(document).scrollTop();
-  //     //when element position starting in viewport
-  //     if (currentPosition > elementViewInBottom && firEvent === false) {
-  //       firEvent = true;
-  //       animationCounter();
-  //     }
-  //   });
-  // }
-
-  // //counter function will animate by using external js also add seprator "."
-  // function animationCounter() {
-  //   $(".number-count").each(function () {
-  //     $(this)
-  //       .prop("Counter", 0)
-  //       .animate(
-  //         {
-  //           Counter: $(this).text(),
-  //         },
-  //         {
-  //           duration: 3000,
-  //           easing: "swing",
-  //           step: function (now) {
-  //             $(this).text(Math.ceil(now));
-  //           },
-  //         }
-  //       );
-  //   });
-  // }
-
  
-  // // Jumping to sections of the same page
-  // $(".arrow-bottomGoSection a").on("click", function (event) {
-  //   event.preventDefault();
-  //   var hash = this.hash;
-  //   $("html, body").animate(
-  //     {
-  //       scrollTop: $(hash).offset().top,
-  //     },
-  //     1000,
-  //     function () {
-  //       window.location.hash = hash;
-  //     }
-  //   );
-  // });
 
   // button scroll to top
   $("#scrollToTop").on("click", function () {
@@ -443,35 +375,7 @@ $(document).ready(function () {
     })
     .trigger("scroll");
 
-  //   // Triger  timeline slider
-  //   $(".custome-timeline").Timeline({
-  //     itemClass: "timeline-item",
-  //     dotsPosition: "top",
-  //     autoplaySpeed: 20,
-  //   });
-  //   var $cols = $(".timeline-horizontal .timeline-dots li");
-  //   var numberOfCols = $cols.length;
-  //   $cols.css("width", 100 / numberOfCols + "%");
-
-  //   // On next click, if the slide-next element exists, trigger the click event on it
-  //   $(".nav-timeline .next").click(function (e) {
-  //     e.preventDefault();
-  //     if ($(".slide-next").length) {
-  //       $(".slide-next").trigger("click");
-  //     }
-  //   });
-
-  //   // Attach a click event listener to elements with the class "nav-timeline" and "prev"
-  //   $(".nav-timeline .prev").click(function (e) {
-  //     // Prevent the default behavior of the click event (i.e. following a link, submitting a form, etc.)
-  //     e.preventDefault();
-
-  //     // Check if there is an element on the page with the class "slide-prev"
-  //     if ($(".slide-prev").length) {
-  //       // If such an element exists, trigger a click event on it
-  //       $(".slide-prev").trigger("click");
-  //     }
-  //   });
+ 
 });
 
 function togglePasswordVisibility(inputId) {
@@ -506,72 +410,3 @@ $(".increment, .decrement").on("click", function (e) {
     input[0][isNegative ? "stepDown" : "stepUp"]();
   }
 });
-// Get the search input element
-// const searchInput = document.querySelector(".search");
-
-// // Get the search list element
-// const searchList = document.querySelector(".search-list");
-
-// // Add event listeners to the search input for clicks, focus, and blur events
-// searchInput.addEventListener("click", toggleSearchList);
-// searchInput.addEventListener("focus", toggleSearchList);
-// searchInput.addEventListener("blur", toggleSearchList);
-
-// // Add a click event listener to the document object
-// document.addEventListener("click", function (event) {
-//   // Check if the click event did not originate from the search input or search list
-//   if (
-//     !searchInput.contains(event.target) &&
-//     !searchList.contains(event.target)
-//   ) {
-//     // Hide the search list by removing the 'show' class
-//     searchList.classList.remove("show");
-//   }
-// });
-
-// // Function to toggle the 'show' class on the search list
-// function toggleSearchList() {
-//   searchList.classList.add("show");
-// }
-
-// const rangeContainer = document.querySelector(".range-container");
-// rangeContainer.addEventListener("input", (ev) => {
-//   const rangeInput = ev.target;
-//   const valueSpan = rangeContainer.querySelector(`#${rangeInput.id}-value`);
-//   const sliderWidth = rangeInput.clientWidth;
-//   const sliderPosition = rangeInput.value;
-//   const sliderMaxValue = rangeInput.max;
-//   const sliderPositionPercentage = (sliderPosition / sliderMaxValue) * 100;
-//   rangeInput.style.backgroundImage = `linear-gradient(to right, #33CA94 ${
-//     (sliderPositionPercentage * sliderWidth) / 100
-//   }px, #B4BEC8 ${(sliderPositionPercentage * sliderWidth) / 100}px)`;
-//   rangeInput.id === "age"
-//     ? (valueSpan.textContent = sliderPosition + " year")
-//     : (valueSpan.textContent = "$" + sliderPosition);
-// });
-// rangeContainer
-//   .querySelectorAll('input[type="range"]')
-//   .forEach((rangeInput) => (rangeInput.value = 0));
-
-// // Set event listener on rangeContainer element
-// rangeContainer.addEventListener("input", (ev) => {
-//   // Declare variables
-//   const rangeInput = ev.target;
-//   const valueSpan = rangeContainer.querySelector(`#${rangeInput.id}-value`);
-//   const sliderWidth = rangeInput.clientWidth;
-//   const sliderPosition = rangeInput.value;
-//   const sliderMaxValue = rangeInput.max;
-//   const sliderPositionPercentage = (sliderPosition / sliderMaxValue) * 100;
-//   // Set rangeInput style
-//   rangeInput.style.backgroundImage = `linear-gradient(to right, #33CA94 ${
-//     (sliderPositionPercentage * sliderWidth) / 100
-//   }px, #f5f5f5 ${(sliderPositionPercentage * sliderWidth) / 100}px)`;
-//   // Set textContent depending on rangeInput.id
-//   rangeInput.id === "age"
-//     ? (valueSpan.textContent = sliderPosition + " year")
-//     : (valueSpan.textContent = "$" + sliderPosition);
-// });
-// // Set all rangeInput values to 0
-// rangeContainer
-//   .querySelectorAll('input[type="range"]')
-//   .forEach((rangeInput) => (rangeInput.value = 0));
