402ec4033bf659380266e3805fad185caa5ec5ab
AT MSI pc <amr-pc@MSI.com>
2024-03-28 02:19:12 +0200

    Signed-off-by: AT MSI pc <amr-pc@MSI.com>

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -102,13 +102,13 @@ use Cake\Routing\Router;
 
 
 <script>
-    // (function(w, d, u) {
-    //     var s = d.createElement('script');
-    //     s.async = true;
-    //     s.src = u + '?' + (Date.now() / 60000 | 0);
-    //     var h = d.getElementsByTagName('script')[0];
-    //     h.parentNode.insertBefore(s, h);
-    // })(window, document, 'https://cdn.bitrix24.com/b16120715/crm/site_button/loader_3_d2o61u.js');
+    (function(w, d, u) {
+        var s = d.createElement('script');
+        s.async = true;
+        s.src = u + '?' + (Date.now() / 60000 | 0);
+        var h = d.getElementsByTagName('script')[0];
+        h.parentNode.insertBefore(s, h);
+    })(window, document, 'https://cdn.bitrix24.com/b16120715/crm/site_button/loader_3_d2o61u.js');
 </script>
 
 
