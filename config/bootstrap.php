65bede5b7d058da8d288124a93d565078ecb4f22
KG <developerkg@thesitefactory.com.au>
2024-03-28 08:37:35 +0200

    checking recaptcha

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -340,7 +340,7 @@ if (!function_exists('getCaptcha')) {
     {
         $Response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . CAPTCHA_SECRET_KEY . "&response={$SecretKey}");
         $Response = json_decode($Response, true);
-        print_r($Response);die;
+        // print_r($Response);die;
         // mail('eng.karimgamal90@gmail.com', 'recaptcha', $Response);
         return ($Response['success'] == true && $Response['score'] > 0.5);
     }
